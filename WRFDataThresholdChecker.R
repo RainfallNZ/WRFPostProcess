821e4aa0bc37e01806407a0336db9048f492b7cd
RainfallNZ <tim.kerr@rainfall.nz>
2020-01-22 22:20:24 +1300

    Improved the formatting of the auto-email body. Changed the smtp server to use noreply@metsolutions.co.nz

--- WRFDataThresholdChecker.R
+++ WRFDataThresholdChecker.R
@@ -8,56 +8,58 @@ if (!require(rdrop2)) install.packages('rdrop2'); library(rdrop2) #Package to en
 if (!require(httpuv)) install.packages('httpuv'); library(httpuv) #Package to enable authenticating DropBox access via drop2
 if (!require(blastula)) install.packages('blastula'); library(blastula) #Package to enable emailing
 if (!require(keyring)) install.packages('keyring'); library(keyring) #Package to enable emailing
-
+if (!require(knitr)) install.packages('knitr'); library(knitr) #Package to enable nice formatting of tables
 
 #A handy function
 minpositive = function(x) min(x[x > 0])
 
-AlertEmail <- function(EmailSubject="Alert Email",EmailContent="Alert test",EmailAlertTable=NULL){
+AlertEmail <- function(EmailSubject="Alert Email",EmailContent="Alert test",EmailAlertTable=cars[1:4,]){
   #A function to send an email
   # Create a simple email message using
   # Markdown-formatted text in the `body`
   # and `footer` sections with the `md()`
   # text helper function
+  FormattedTable <- kable(EmailAlertTable, format="html")
   email <-
     compose_email(
-      body = md(paste(EmailContent,"
-      
-      ",EmailAlertTable,"
+      body = md(c(EmailContent,"***",
+      {FormattedTable},"
 
-This is an automated alert Email sent on behalf of
+***      
+This is an automated alert Email sent on behalf of:
 
-Mike Green
-
-Engineering Meteorologist
-
-Meteorology Solutions Ltd.
-
-T:021 249 9248
+Mike Green  
+Engineering Meteorologist  
+Meteorology Solutions Ltd.  
+T:021 249 9248  
 
 mike.green@metsolutions.co.nz
 ")),
       footer = md(
         "
-metsolutions.co.nz
+[metsolutions.co.nz](https://metsolutions.co.nz)
 ")
     )
   
   # The email message can always be
   # previewed by calling the object
-  #if (interactive()) email
+  if (interactive()) email
   
   ##One time setup of a key pair to enable automated email sending
+  ##Note that there is a bug in the Blatsula package so that only one smtp server key can be held at a time
+  ##seehttps://github.com/rich-iannone/blastula/issues/118
+  ##keys can be listed using key_list()
+  ##keys can be deleted using key_delete(service="sfsdfd",username="sdfsdfsd")
   #create_smtp_creds_key(id="TimsAutoEmailKey",user="tim.kerr@rainfall.nz",host="mailx.freeparking.co.nz",port=465, use_ssl = TRUE)
+  #create_smtp_creds_key(id="MikesAutoEmailKey",user="noreply@metsolutions.co.nz",host="mail.1stdomains.co.nz",port=465, use_ssl = TRUE)
   
   email %>%
     smtp_send(
-      from = "tim.kerr@rainfall.nz",
+      #from = "tim.kerr@rainfall.nz",
+      from = "noreply@metsolutions.co.nz",
       to = "timkerr37@hotmail.com",
       subject = EmailSubject,
-      credentials = creds_key(
-        "TimsAutoEmailKey"
-      )
+      credentials = creds_key("MikesAutoEmailKey")
     )
 }
 
@@ -95,7 +97,7 @@ SiteIndex <- which(SiteAttributes$SiteNames == SiteName)
 #Figure out the filename of the latest file of interest
   
 #Get a list of all the files
-DropBoxFiles <- drop_dir(path="Rainfall.NZ/WRF_data/" )
+DropBoxFiles <- drop_dir(path="Rainfall.NZ/WRF_data/")
 
   #Find which ones have a csv suffix
 FilesOfInterest <- which(endsWith(DropBoxFiles$path_display,".csv"))
@@ -107,7 +109,7 @@ MetadataOfFilesOfInterest <- lapply(DropBoxFiles$path_lower[FilesOfInterest], dr
 DatesOfFilesOfInterest <- sapply(MetadataOfFilesOfInterest, "[[", "client_modified")
 
 #Set the datetimes as date-time objects
-Dates <- as.POSIXct(DatesOfFilesOfInterest)
+Dates <- as.POSIXct(DatesOfFilesOfInterest, format="%Y-%m-%dT%H:%M:%SZ",tz="utc")
 
 #Find which of the csv files has the latest date-time
 MaxDateIndex <- which(Dates == max(Dates))
@@ -139,6 +141,7 @@ if (length(ThresholdedIndices) > 0){
   #Convert the data to NZ time zone
   ThresholdedDateTimes <- as.POSIXct(paste(SiteSpecificData[ThresholdedIndices,'Date'],SiteSpecificData[ThresholdedIndices,'Time']),tz="utc")
   attr(ThresholdedDateTimes, "tzone") <- "nz"
+  ThresholdedDateTimes <-  format(ThresholdedDateTimes, "%d %b at %I %p")
   
   #Select the data of interest
   ThresholdedData <- round(SiteSpecificData[ThresholdedIndices,Parameter],1)
@@ -146,9 +149,9 @@ if (length(ThresholdedIndices) > 0){
   #Prepare the information for the email
   EmailSubject <- paste(SiteAttributes$SiteLongNames[SiteIndex], if(Above) {"high"} else {"low"}, ParameterAttributes$ParameterLongNames[ParameterIndex],"notification")
   
-  EmailMessage <- paste("The",ParameterAttributes$ParameterLongNames[ParameterIndex],"is forecast to drop below",Threshold,ParameterAttributes$Units[ParameterIndex], "at the following times over the next two days") 
+  EmailMessage <- paste("The",ParameterAttributes$ParameterLongNames[ParameterIndex],"is forecast to drop below",Threshold,ParameterAttributes$Units[ParameterIndex], "at the following times over the next three days") 
   
-  EmailTable <- data.frame('Date Time' = ThresholdedDateTimes, Parameter = ThresholdedData)
+  EmailTable <- data.frame('Time' = ThresholdedDateTimes, Parameter = ThresholdedData,check.names = FALSE)
   names(EmailTable)[2] <- ParameterAttributes$ParameterLongNames[ParameterIndex] 
   
   #Call a yet-to-be-written email function
